f84b4f23ee60550d546e56492c31c09512e41e7a
mingshrp <mingmashrp23@gmail.com>
2026-02-15 23:06:22 +0000

    Clean up

--- homework1/tests/test_task2.py
+++ homework1/tests/test_task2.py
@@ -1,4 +1,8 @@
 import pytest
 from src.task2 import integer_type, float_type, string_type, bool_type
 
-def test_integer():
+def test_integer(): 
+    assert isinstance(integer_type(), int)
+    assert integer_type() == 10
+
+    

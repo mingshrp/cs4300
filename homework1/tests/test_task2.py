f7188d56d67474d98e2bddb5046df56b2f353663
mingshrp <mingmashrp23@gmail.com>
2026-02-15 23:50:07 +0000

    Write up test

--- homework1/tests/test_task2.py
+++ homework1/tests/test_task2.py
@@ -1,8 +1,8 @@
 import pytest
 from src.task2 import integer_type, float_type, string_type, bool_type
 
-def test_integer(): 
+def test_integer_type():
     assert isinstance(integer_type(), int)
-    assert integer_type() == 10
+    assert integer_type() == 1234
 
     

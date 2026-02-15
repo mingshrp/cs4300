07e6de7e2413db07212289b40d17aa5234078aa4
mingshrp <mingmashrp23@gmail.com>
2026-02-15 22:45:04 +0000

    Update repo and add task2

---
+++ homework1/src/task2.py
@@ -0,0 +1,14 @@
+# Various data types
+
+def integer_type():
+    return 1234
+
+def float_type():
+    return 3.14
+
+def string_type():
+    return "Hello, World!"
+
+def bool_type():
+    return False
+
---
+++ homework1/tests/test_task1.py
@@ -0,0 +1,4 @@
+import pytest
+from src.task2 import integer_type, float_type, string_type, bool_type
+
+def test_integer():

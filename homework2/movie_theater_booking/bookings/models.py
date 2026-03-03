b0e760e207a35994d314b3e999a038efc705c73c
mingshrp <mingmashrp23@gmail.com>
2026-03-03 02:49:37 +0000

    Create models for app

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+    release_date = models.DateField()
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+
+    def __str__(self):
+        return self.title
+
+class Seat(models.Model):
+    seat_number = models.CharField(max_length=10, unique=True)
+    is_booked = models.BooleanField(default=False) # False = Available 
+
+    def __str__(self):
+        return self.seat_number
+
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="bookings")
+    seat = models.OneToOneField(Seat, on_delete=models.CASCADE, related_name="booking") # prevent double booking (each seat = one booking)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="bookings")
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} booked {self.seat} for {self.movie}"

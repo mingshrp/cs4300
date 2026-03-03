8061f16b4e4df73e651fab2f2b3119f124f8b22d
mingshrp <mingmashrp23@gmail.com>
2026-03-03 17:37:56 +0000

    Add template views

--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import viewsets, status, permissions 
 from rest_framework.response import Response
 
@@ -67,3 +67,60 @@ class BookingViewSet(viewsets.ModelViewSet):
         seat.save()
 
         return Response(self.get_serializer(booking).data, status=status.HTTP_201_CREATED)
+
+
+# ======== Views for HTML Template Pags ============
+
+def movie_list(request):
+    """
+    Display list of all movies.
+
+    param: 
+        request (HttpRequest): The incoming HTTP request object.
+    return:
+        HttpResponse: Rendered HTML page displaying all movies from the database.
+    """
+    movies = Movie.objects.all()
+    return render(request, "bookings/movie_list.html", {"movies": movies})
+
+def seat_booking(request, movie_id):
+    """
+    Display seat availability for a selected movie.
+
+    params:
+        request (HttpRequest): The incoming HTTP request object.
+        movie_id: ID of the selected movie.
+
+    return:
+        HttpResponse: HTML page showing seat numbers and booking status for movie.
+
+    ERROR:
+        Http404: If a movie with the given ID does not exist.
+    """
+    movie = get_object_or_404(Movie, id=movie_id)
+    seats = Seat.objects.all()
+
+    return render(request, "bookings/seat_booking.html", {
+        "movie": movie, 
+        "seats": seats
+    })
+
+def booking_history(request):
+    """
+    Show booking history for user.
+
+    param:
+        request (HttpRequest): The incoming HTTP request object.
+
+    return:
+        HttpResponse: HTML page showingvuser's booking history. 
+    """
+    if request.user.is_authenticated:
+        bookings = Booking.objects.filter(user=request.user)
+    else:
+        bookings = [] # pass empty list if user is not authenticated
+
+    return render(request, "bookings/booking_history.html", {
+        "bookings": bookings
+    })
+

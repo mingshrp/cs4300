4d3542d8afadab79b5308b9b4c3aec38730345e6
mingshrp <mingmashrp23@gmail.com>
2026-03-03 08:48:43 +0000

    Set up DRF serializers

---
+++ homework2/movie_theater_booking/bookings/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers  
+from .models import Movie, Seat, Booking
+# https://www.w3tutorials.net/blog/django-rest-framework-use-different-serializers-in-the-same-modelviewset/#understanding-modelviewset 
+
+# Define serializer classes: map Django models to JSON representations and validate API endpoints
+
+class Meta:
+        # Use movie model from models.py
+        model = Movie
+        fields = ['id', 'title', 'description', 'release_date', 'duration']
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = ['id', 'seat_number', "is_booked"]
+        # is_booked is determined by booking logic, users cannot manually change it
+        read_only_fields = ['is_booked']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['id', 'movie', 'seat', 'user', 'booking_date']
+        # not allowed to manually set booking date 
+        read_only_fields = ['booking_date']
--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-
+from rest_framework import viewsets, status, permissions 
+from rest_framework.response import Response
+from .models import Movie, Seat, Booking
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
 # Create your views here.
